0fbd4e617f6272716d95c363122cad88ce67825d
diawara ismael <isamepro@hotmail.com>
2016-02-17 19:34:52 -0500

    initial

---
+++ Serikpa1/Serikpa1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Serikpa1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Serikpa1/Serikpa1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Serikpa1.WebApiApplication" Language="C#" %>
